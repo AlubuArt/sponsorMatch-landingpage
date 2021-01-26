726586af856cf1c95d59153cb04a9b214bbd02b1
AlubuArt <57637214+AlubuArt@users.noreply.github.com>
2021-01-26 20:00:07 +0100

    om os section added

--- src/sections/om-os/om-os.js
+++ src/sections/om-os/om-os.js
@@ -1,6 +1,7 @@
 import react from 'react';
 import {Container, Row, Col} from 'react-bootstrap';
 import './om-os.css';
+import profilePeter from  '../../media/profilePIctureHeader.png';
 
 
 
@@ -18,6 +19,22 @@ const OmOs = () => {
                         om teamet
                     </h1>
                     <h2>og hvorfor vi kan styrke foreningsdanmark</h2>
+                </Col>
+
+            </Row>
+            <Row>
+                <Col className="profile-pictures">
+                    <img  src={profilePeter} alt=""></img>
+                    < h2 className="profile-title">Peter Overby, Co-founder & CEO</h2>
+                    <p className="profile-description">Med flere års erfaring fra DBU som klubudvikler og klubkonsulent, har Peter fingeren på pulsen og er en rigtig foreningsmand</p>
+               
+                    <img  src={profilePeter} alt=""></img>
+                    < h2 className="profile-title">Jacob Christensen, Co-founder & CTO</h2>
+                    <p className="profile-description">Med flere års erfaring fra DBU som klubudvikler og klubkonsulent, har Peter fingeren på pulsen og er en rigtig foreningsmand</p>
+                
+                    <img  src={profilePeter} alt=""></img>
+                    < h2 className="profile-title">Kristoffer, Co-founder & Account Manager</h2>
+                    <p className="profile-description">Med flere års erfaring fra DBU som klubudvikler og klubkonsulent, har Peter fingeren på pulsen og er en rigtig foreningsmand</p>
                 </Col>
             </Row>
         </Container>
