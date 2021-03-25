3216bc25d815c9d9d86f858e86c4393238b6cee2
AlubuArt <jc@jcvisueldesign.dk>
2021-03-25 09:06:58 +0100

    media query 1200 done

--- src/sections/hero/hero.js
+++ src/sections/hero/hero.js
@@ -15,19 +15,21 @@ const Hero = () => {
                     <h2>Vi hjælper breddeforeninger til flere indtægter med DK´s eneste</h2>
                     <h1>TOTALLØSNING</h1>
                     <h2>der gør sponsorarbejdet lettere ved at samle alt et sted!</h2>
-                </div>
-                <div>
+                </div> <div>
                     <p>Kom igang med det samme helt gratis -  og få med det samme 3 forslag til nye sponsorer som passer ind i din forening</p>
                 </div>  
                 <Button className="hero-button">Prøv SponsorMatch - GRATIS </Button>
-              
             </Col>
-            <Col>
+                <Col>
                 <div className="hero-img">
                     <img src={heroImage} className="hero-img" alt=""></img>
                 </div>
-                
+               
             </Col>
+                
+              
+            
+           
            
         </Jumbotron>
     )
